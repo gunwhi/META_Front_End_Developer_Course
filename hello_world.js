a68b698835b8c41db335f8dbf263ab3da2229261
gunwhi <102884128+gunwhi@users.noreply.github.com>
2022-12-02 15:00:31 -0800

    Add files via upload

---
+++ hello_world.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+// this is a comment
+
+/*
+multiple
+line
+comment
+*/
+
+/*or this is a multi-line single line comment*/
+
+
+console.log("%cHello, World", "color: blue; font-size: 40px");
+console.log("Hello " + "there, " + "World");
+console.log("Hello ", "there, ", "World");
+
+
+// declaring variable
+var person;
+person = "john";
+console.log("hello", person);
+var greeting = "hello";
+person = "james";
+console.log(greeting, person);
